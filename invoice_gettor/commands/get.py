b2dd12778eddc9b7b8dce91fe8621a09acf020ca
Vincent <yuke@cs.stonybrook.edu>
2024-07-28 15:00:31 -0400

    Add output for each step

--- invoice_gettor/commands/get.py
+++ invoice_gettor/commands/get.py
@@ -20,18 +20,32 @@ async def get(gpt, actual, no_pdf, debug, order_id):
 
 
         order_total, grand_total, items = await sc.getInvoice(order_id)
-        print(items)
-        print(order_total)
+        print("Items in invoice:")
+        for i, names in enumerate(items):
+            print(f"{i}. {names}")
+        print("--------------------------------")
+        print(f"Order total: {order_id}  Grand total: {grand_total}")
 
         if gpt:
             gptw = GPTWrapper()
             shortened_names = gptw.summarizeOrder(items)
         
-        print("Summarized items:")
-        print(shortened_names)
+            print("Summarized items:")
+            for i, names in enumerate(shortened_names):
+                print(f"{i}. {names}")
+            print("--------------------------------")
 
         if actual:
-            notes = ", ".join(shortened_names) + " | " + order_id
+            if gpt:
+                notes = ", ".join(shortened_names) + " | " + order_id
+            else:
+                notes = order_id
+
+            print("New Actual transaction:")
             print(notes)
-            with ActualWrapper() as aw:
-                aw.addOrder(notes=notes, payment=decimal.Decimal("-"+order_total[1:]), date="", category="")
+            confirmation = input("Add this transaction? (yes/no):")
+            if confirmation.lower() == 'yes' or confirmation.lower() == 'y':
+                with ActualWrapper() as aw:
+                    aw.addOrder(notes=notes, payment=decimal.Decimal("-"+order_total[1:]), date="", category="")
+            else:
+                pass
